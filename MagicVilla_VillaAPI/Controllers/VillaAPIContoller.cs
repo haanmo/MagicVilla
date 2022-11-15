662b6631271ef098fa9e4911210a9b22b87b6ddd
haanmo <haanmo@naver.com>
2022-11-14 22:24:13 -0500

    add an api

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIContoller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+	[Route("api/VillaAPI")]
+	[ApiController]
+	public class VillaAPIContoller : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<Villa> GetVillas()
+		{
+			return new List<Villa>
+			{
+				new Villa{Id=1, Name="Pool View"},
+				new Villa{Id=2, Name="Beach View"}
+			};
+		}
+
+		public VillaAPIContoller()
+		{
+		}
+	}
+}
+
---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MagicVilla_VillaAPI.Models
+{
+	public class Villa
+	{
+		public int Id { get; set; }
+		public String Name { get; set; }
+
+		public Villa()
+		{
+		}
+	}
+}
+
